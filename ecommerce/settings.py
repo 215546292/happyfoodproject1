4add81826ef03ca21cd4852a5d6342b34e416025
unknown <215546292@tut4life.ac.za>
2026-02-19 14:21:41 +0200

    Add Render domain to ALLOWED_HOSTS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+ba#oegqrxjao2($_x=3g+to^p%f5seekut0ml1mo8v9va*d%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
